73a095ef695bc07d9415f08595a443575a2b3a47
Luis Miguens Fernandez <luis.miguens@cern.ch>
2026-02-17 13:44:54 +0100

    Update more human friendly names

--- retrieve-jobs.py
+++ retrieve-jobs.py
@@ -1,7 +1,6 @@
 from common import *
 import os
 import json
-import re
 import gitlab
 
 PIPELINE_ID = os.environ.get("PIPELINE_ID", None)
@@ -17,20 +16,13 @@ jobs = pipeline.jobs.list(all=True)
 
 jobs_list = []
 
-def build_github_key(job_name: str, job_id: str) -> str:
-    raw_key = f"{job_name}_{job_id}"
-    key = re.sub(r"[^\w-]", "_", raw_key)
-    key = re.sub(r"_+", "_", key)
-    key = key.strip("_-")
-    if not key:
-        key = f"job_{job_id}"
-    return key
-
+counter = 1
 
 for i in jobs:
     job_id = str(i.id)
     job_name = str(i.name)
-    key = build_github_key(job_name, job_id)
+    key = counter
+    counter += 1
     jobs_list.append(
         {
             "name": job_name,
