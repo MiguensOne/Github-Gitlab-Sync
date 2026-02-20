3583f5ab9b8d16f3d9e414c23f24a4d5075e22e0
Luis Miguens Fernandez <luis.miguens@cern.ch>
2026-02-20 16:21:21 +0100

    Print URL for manual job and not wait for logs

--- retrieve-job.py
+++ retrieve-job.py
@@ -12,9 +12,14 @@ project = gl.projects.get(PROJECT_ID)
 
 job = project.jobs.get(JOB_ID)
 
-while job.status not in ["success", "failed", "canceled", "skipped"]:
+while job.status not in ["success", "failed", "canceled", "skipped", "manual"]:
     time.sleep(POLL_INTERVAL)
     job.refresh()
+
+if job.status == "manual":
+    print("Job is in manual state. Please trigger it manually:")
+    print(job.web_url)
+    exit(0)
 
 log_binary = job.trace()
 log_decoded = log_binary.decode("utf-8")
